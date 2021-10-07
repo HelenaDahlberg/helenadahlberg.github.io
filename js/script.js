7c96f552739d32dbc89fc00a5422fc069c6d250a
Helena <nena_haggstrom@hotmail.com>
2021-10-07 16:22:05 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+  function change() 
+  {
+      var elem = document.getElementById("myButton1");
+      if (elem.value=="Close") elem.value = "More";
+      else elem.value = "Close";
+  }
+  
+  function change() 
+  {
+      var elem = document.getElementById("myButton2");
+      if (elem.value=="Close") elem.value = "More";
+      else elem.value = "Close";
+  }
+
+  function change() 
+  {
+      var elem = document.getElementById("myButton3");
+      if (elem.value=="Close") elem.value = "More";
+      else elem.value = "Close";
+  }
+
+  function change() 
+  {
+      var elem = document.getElementById("myButton4");
+      if (elem.value=="Close") elem.value = "More";
+      else elem.value = "Close";
+  }
+
+  function change() 
+  {
+      var elem = document.getElementById("myButton5");
+      if (elem.value=="Close") elem.value = "More";
+      else elem.value = "Close";
+  }
+
+  
